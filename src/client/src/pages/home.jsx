6b6e9245297f65d221b51ad8bf724c87f39e142a
Rishbah Jain <rishabhjainfinal@gmail.com>
2021-09-14 16:20:29 +0530

    updates 😄

--- src/client/src/pages/home.jsx
+++ src/client/src/pages/home.jsx
@@ -7,36 +7,60 @@ import MovieCard from '../components/MovieCard';
 // import * as Types from "../redux/types";
 import Key from "../redux/keys";
 
-
-// this is just for the shows
-// https://api.tvmaze.com/shows // update it for movies also
-
-
-const searchByNameURI = (name) => `https://api.tvmaze.com/search/shows?q=${name}`
-const searchByNameURI2 = (api_key, name) => `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${name}`
+// const searchByNameURI = (name) => `https://api.tvmaze.com/search/shows?q=${name}`
+const searchByNameURI = (name, api_key) => `https://api.themoviedb.org/4/search/movie?api_key=${api_key}&query=${encodeURI(name)}`
 
 function Home({ }) {
     const [SearchValue, setSearchValue] = useState('');
     const [JsonResponse, setJsonResponse] = useState([]);
+    const [APIKey, setAPIKey] = useState('')
+
+    function fetchByName(value, APIkey = APIKey) {
+        var URI = searchByNameURI(value, APIkey)
+        // console.log(URI, value)
+        if (value) {
+            return fetch(URI)
+                .then(res => res.json())
+                .then(res => {
+                    try {
+                        console.log(res.results);
+                        setJsonResponse(res.results)
+                    } catch {
+                        console.log(`1234`);
+                        setJsonResponse([])
+                    }
+                })
+                .catch(err => console.log(err))
+        } else {
+            return
+        }
+    }
 
     useEffect(() => {
-        console.log(
-
-            Key.get()
-        );
+        // test the key here
+        setAPIKey(Key.get())
+        // for (let index = 0; index < 3; index++) {
+        //     var testKey = Key.get()
+        //     fetchByName("avengers", testKey)
+        //     if (JsonResponse) {
+        //         setJsonResponse([])
+        //         setAPIKey(testKey)
+        //         break
+        //     } else {
+        //         Key.del(testKey);
+        //     }
+        // }
+        setJsonResponse([])
+        return () =>{
+            setJsonResponse([])
+        }
     }, [])
 
-    function fetchByName(value) {
-        fetch(searchByNameURI(value))
-            .then(res => res.json())
-            .then(res => setJsonResponse(res))
-            .catch(err => console.log(err))
-    }
     return (
         <div className="Home">
             <div className="head">
                 <h1 className={SearchValue.trim() ? "headName small" : "headName big"} >Torrent Search</h1>
-                <nav className={SearchValue ? "officialNav" : "empty"}>
+                <nav className={SearchValue.trim() ? "officialNav" : "empty"}>
                     <TextField
                         autoFocus
                         size='medium'
@@ -48,26 +72,33 @@ function Home({ }) {
                         variant="outlined"
                         onChange={(event) => {
                             setSearchValue(event.target.value);
-                            fetchByName(SearchValue);
+                            fetchByName(event.target.value, APIKey);
+                            if (event.target.value == ""){
+                                setJsonResponse([])
+                            }
+                            console.log(JsonResponse);
                         }}
                     />
                 </nav>
                 {SearchValue && <ShowInputValue value={SearchValue} />}
             </div>
             <div className="showCards">
-                <div className="showMovies">
-                    "this is the way"
-                    {
-                        SearchValue
-                        && JsonResponse
-                            .map(
-                                element => {
-                                    console.log(element);
-                                    return <h1 >{element.show.name}</h1>
-                                }
-                            )
-                    }
-                </div>
+                {
+                    SearchValue.trim() != "" &&
+                    JsonResponse &&
+                    JsonResponse.map(
+                        element => {
+                            if (element.poster_path) {
+                                return <MovieCard key={element.id} {...element} />
+                            }
+                        }
+                    )
+                }
+                {
+                    (SearchValue.trim() != "") 
+                    && JsonResponse.length == 0 
+                    && <h1> 😟 Noting to show ¯\_(ツ)_/¯</h1>
+                }
             </div>
         </div>
     )
