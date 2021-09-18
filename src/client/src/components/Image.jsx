46e61d89130a75c1a9c41e3a5a78a58d4adfbd8f
Rishbah Jain <rishabhjainfinal@gmail.com>
2021-09-18 21:55:01 +0530

    Image final 😄

--- src/client/src/components/Image.jsx
+++ src/client/src/components/Image.jsx
@@ -1,19 +1,18 @@
-import React, { useState, Suspense } from 'react'
-import { Img } from 'react-image'
+import React, { useState, Suspense } from 'react';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
-
-// make image loader spinner
 
 export default function Image({ src, alt }) {
     const [isloaded, setIsloaded] = useState(false);
     return (
-        <Img
-            src={src}
-            alt={alt}
-            onLoad={() => { setIsloaded(true) }}
-            width='200'
-            height='300'
-            loader={<p>this is just for the testing</p>}
-        />
+        <>
+            <img
+                className={isloaded ? "ImageItself " : "displayNone"}
+                src={src}
+                alt={alt || "Poster for the movie"}
+                onLoad={() => setIsloaded(true)}
+            />
+            {!isloaded && <CircularProgress color="secondary" />}
+        </>
     )
 }
