e7679d96ee34ce6444abf88a45bed87aada1f3ca
Rishbah Jain <rishabhjainfinal@gmail.com>
2021-09-18 21:56:25 +0530

    Movie card done😄
    need some work on the pading and text formating

--- src/client/src/components/MovieCard.jsx
+++ src/client/src/components/MovieCard.jsx
@@ -2,29 +2,28 @@ import React, { useEffect, Suspense } from 'react'
 import Image from '../components/Image';
 import "../css/MovieCard.css";
 
-function MovieCard({ title, id, poster_path, vote_average }) {
-
-    // show name 
-    // and pic in between and
-    // some discription and starts info at the bottom 
-    // take it from itretor
+function MovieCard(props) {
+    const { title, poster_path, overview , vote_average } = props;
     const src = "https://image.tmdb.org/t/p/w200/" + poster_path
     return (
         <div className="aCard">
-            {/* <h3 key={id} >{title}</h3>
-            <Image src={src} /> */}
             <div className="smallPost">
                 <div className="imageCointainer">
-                    <img src={src} alt="random post" />
+                    <Image src={src} alt="random post" />
                 </div>
                 <div className="textConintainer">
-                    <h2 className="heading">{title}</h2>
-                    <h5 className="content">
-                        {"text"}
-                    </h5>
-                    <div className="moreintext bolt">
-                        ...more
-                    </div>
+                    <h3 className="heading">
+                        {title}
+                    </h3>
+                    <span className="content">
+                        {overview}
+                    </span>
+                    <span className="baseline bolt">
+                        {vote_average}/10
+                        <span className="moreintext ">
+                            ...more
+                        </span>
+                    </span>
                 </div>
             </div>
         </div>
