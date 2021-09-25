466e72d8c940f7284d92e414f3dbde2d21cadec8
Rishbah Jain <rishabhjainfinal@gmail.com>
2021-09-25 22:21:04 +0530

    created new page
    to list the torrent files here

--- src/client/src/pages/ShowTorrent.jsx
+++ src/client/src/pages/ShowTorrent.jsx
@@ -1,19 +1,49 @@
-import React,{useEffect} from 'react'
-import * as Types from "../redux/types";
-import { store, updateMagnet } from "../redux/store";
+import React, { useEffect } from 'react'
+import { updateMagnet, getMagnet } from "../redux/store";
+import queryString from 'query-string';
+import { useParams } from "react-router-dom";
 
+// update the magnet
+// show selected torrent
+// console.log(getMagnet())
+
+
+// function to get the paramer NAME value from the url 
+function getParamName() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const page_type = urlParams.get('name');
+    // get its value and if not return ''   
+    return page_type || '';
+}
 
 function ShowTorrent() {
     useEffect(() => {
-        // get the movie name from the store ele redirect to the home 
+        // updateMagnet(" new magnet here 1");
+
+        // redirect the page if name param not found
+        if (getParamName().length == 0) {
+            document.location.href = '/'
+        }
         return () => {
         }
     }, [])
+
     return (
-        <div>
-            this is torrent show page
-            {store.toString()}
-        </div>
+        <>
+            <div className="headingName">
+                {getParamName()}
+            </div>
+            <table>
+                {/* map here to  */}
+                <td>
+                    <th>count</th>
+                    <th>Name</th>
+                    <th>seeder</th>
+                    <th>reciever</th>
+                </td>
+            </table>
+        </>
     )
 }
 
